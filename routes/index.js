7cf5bbca56f3a8b92b26743682e1d8e0ba830075
Mansi <mpatel.y@gmail.com>
2016-02-07 09:39:10 -0500

    update background

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,29 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express on azure' });
+ // res.render('index', { title: 'Express' });
+ res.render('index');
 });
+
+//About get request
+router.get('/about', function(req, res, next) {
+  res.render('about');
+});
+
+//Services get request
+router.get('/services', function(req, res, next) {
+  res.render('services');
+});
+
+//Projects get request
+router.get('/projects', function(req, res, next) {
+  res.render('projects');
+});
+
+//About get request
+router.get('/contact_me', function(req, res, next) {
+  res.render('contact_me');
+});
+
 
 module.exports = router;
